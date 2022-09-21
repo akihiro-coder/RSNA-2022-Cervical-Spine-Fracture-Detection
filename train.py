4ed63eaf80237d8d027cf4816044581409aa2598
AkihiroOkuda <ScuderiaFerrariSebastian@gmail.com>
2022-09-21 23:25:36 +0900

    second commit

--- train.py
+++ train.py
@@ -1,1 +1,23 @@
+import gc 
+import glob
+import os
+import re
+
+
+import cv2
+import matplotlib.pyplot as plt 
+import numpy as np
+import pandas as pd
+import pydicom as dicom
 import torch
+import torchvision as tv
+from sklearn.model_selection import GroupKFold
+from torch.cuda.amp import GradScaler, autocast
+from torchvision.models.feature_extraction import create_feature_extractor
+from tqdm.notebook import tqdm
+
+
+import wandb
+
+
+
